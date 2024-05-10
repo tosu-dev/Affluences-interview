4c8267e9acc8e19103cef44610d9f1306c25cecb
Théo Constant <59777055+tosu-dev@users.noreply.github.com>
2024-05-11 01:25:30 +0200

    v2 fonctionnel
    Fait en ~30 minutes en plus.

--- main.py
+++ main.py
@@ -2,25 +2,42 @@ from fastapi import FastAPI
 
 from data import *
 
-
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+  return {"message": "Hello World"}
 
 
 @app.get("/site/{site_id}")
 async def get_site_history_by_id(site_id: int, start_datetime: datetime, end_datetime: datetime):
-    json = {"site_id": site_id, "data": [], }
+  """
+  Get the history if a site by its id between two datetime with data every half an hour
+  :param site_id: site's id
+  :param start_datetime: beginning datetime
+  :param end_datetime: end datetime
+  :return: json
+  """
+  # Basic json to return
+  return_json = {"site_id": site_id, "data": [], }
 
-    all_site_df = get_df_every_half_hour(site_id, start_datetime, end_datetime)
+  current_datetime = start_datetime
+  all_site_df = get_df_every_half_hour(site_id, start_datetime, end_datetime)
 
-    json["data"].append(df_half_hour_to_json(all_site_df[0]))
-    old_site_df = all_site_df[0]
+  if len(all_site_df) > 0:
+    # Init first time
+    old_json = df_half_hour_to_json(all_site_df[0], current_datetime)  # Keep old json result
+    return_json["data"].append(old_json)  # Put it in the return json
+    current_datetime += timedelta(minutes=30)  # Add 30 minutes to current datetime
+
+    # Do this for every other DataFrame
     for site_df in all_site_df[1:]:
-        json["data"].append(df_half_hour_to_json(site_df, old_site_df))
-        old_site_df = site_df
-    return json
+      site_json = df_half_hour_to_json(site_df, current_datetime,
+                                       total_entries=old_json["cumulated_entries"],
+                                       total_exists=old_json["cumulated_exits"])
+      return_json["data"].append(site_json)
+      old_json = site_json
+      current_datetime += timedelta(minutes=30)
 
+  return return_json
